03eb6bc21e424cce3bb1d51e1bc37edbde9d94ce
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2016-05-05 02:44:53 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,9 @@ var bowerFiles = require('main-bower-files');
 
 elixir(function(mix) {
     mix.sass('app.scss');
-//    mix.scripts(bowerFiles('**/*.js'), 'public/js/3rd-party.js', '/');
     mix.scripts([
                 'jquery/dist/jquery.min.js',
                 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
-],'public/js/3rd-party.js', 'bower_components');
+                ],'public/js/3rd-party.js', 'bower_components');
 });
 
