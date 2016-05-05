2e1e2db84fe17a5e5d44bbfb68c9c933825a9ff9
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2016-05-05 15:43:09 +0500

    added queue

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel Apz</title>
+        <title>Laravel Ajax</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="/css/app.css">
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>Laravel Aax</h1>
+        <h1>Laravel Ajax</h1>
         <p>The guide to build a Laravel 5.2 app from a to z.</p>
         <p>
             <a href="https://github.com/jp7internet/laravel-apz" role="button" class="btn btn-lg btn-primary">View On GitHub</a>
